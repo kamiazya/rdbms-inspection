df1ba8bd8781ae4f574679a893e836847b2e717d
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2021-11-03 15:32:51 +0000

    fix ci

--- libs/db/mysql.py
+++ libs/db/mysql.py
@@ -1,9 +1,14 @@
 "Database utils for MySQL"
+import os
+
 from MySQLdb.connections import Connection
+
+
+MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'mysql'
 
 
 def connect() -> Connection:
     "Connect to MySQL and return Connection object."
     return Connection(
-        host="mysql", port=3306, user="mysql", password="mysql", database="db"
+        host=MYSQL_HOST, port=3306, user="mysql", password="mysql", database="db"
     )
